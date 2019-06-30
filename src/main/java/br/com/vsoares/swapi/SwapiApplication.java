471a3f92714479134f505c4b80ea90a6441b4a8f
Victor <victor.vsoares@gmail.com>
2019-06-30 18:48:49 -0300

    Adicionando o circuit breaker

--- src/main/java/br/com/vsoares/swapi/SwapiApplication.java
+++ src/main/java/br/com/vsoares/swapi/SwapiApplication.java
@@ -3,6 +3,7 @@ package br.com.vsoares.swapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories
 @EnableTransactionManagement
 @EnableFeignClients
+@EnableCircuitBreaker
 public class SwapiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/vsoares/swapi/client/SWApiClient.java
+++ src/main/java/br/com/vsoares/swapi/client/SWApiClient.java
@@ -1,10 +1,11 @@
 package br.com.vsoares.swapi.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="swapi", url = "https://swapi.co/api/")
+@FeignClient(name="swapi", url = "https://swapi.co/api/", fallback = SWApiClient.SWApiFallback.class)
 interface SWApiClient {
 
 	int PAGE_SIZE = 10;
@@ -15,4 +16,19 @@ interface SWApiClient {
 	@GetMapping("planets/?page={page}")
 	SWApiResult findAllPlanets(@PathVariable("page") int page);
 	
+	@Component
+	static  class SWApiFallback implements SWApiClient {
+		
+		@Override
+		public SWApiResult findPlanet(String name) {
+			return new SWApiResult();
+		}
+		
+		@Override
+		public SWApiResult findAllPlanets(int page) {
+			return new SWApiResult();
+		}
+		
+	}
 }
+
